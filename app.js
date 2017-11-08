df02ed2d415b4b5e4e2026edbc4969cdf4774312
Jesus Mendoza <je.mendoza@live.com>
2017-11-08 10:46:46 -0800

    removed grayed out section of code in java file

--- app.js
+++ app.js
@@ -101,20 +101,3 @@ while (count !=5) {
 }
 
 alert("all Done! Great Job!")
-
-
-
-
-//var kids = prompt('how many kids do you have?');
-//console.log('number of kids:', kids);
-
-//var hats = prompt('how many hats do you own?');
-//console.log('number of hats:', hats);
-
-//if (kids === '4' || kids === '3') {
-  //alert('nice job - u right');
-//}
-
-//if (hats === '3' && kids === '4') {
-  //alert('nice job again - u rule');
-//}
